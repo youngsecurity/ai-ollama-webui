f9a7e4bf209e5d34396a32c31384e42fb3571abd
Joseph Young <130690493+yousecjoe@users.noreply.github.com>
2024-03-21 00:10:44 -0400

    Updated web interface title to 'Zero Trust GPT'
    Changed the name of the web interface in the configuration to more accurately reflect the current project branding. This aligns the user-facing title with the new 'Zero Trust GPT' theme.

--- backend/config.py
+++ backend/config.py
@@ -23,7 +23,7 @@ try:
 except ImportError:
     print("dotenv not installed, skipping...")
 
-WEBUI_NAME = "Open WebUI"
+WEBUI_NAME = "Zero Trust GPT"
 #shutil.copyfile("../build/favicon.png", "./static/favicon.png")
 
 ####################################
